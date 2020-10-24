d173914fce7140447acef1266e1bc4cf5e1cf289
Robert M. Estrada <57651772+robertestrada@users.noreply.github.com>
2020-10-23 22:12:43 -0700

    Add navbar cart button and page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,11 +5,11 @@ import '../styles/navBar.css';
 import { useDispatch } from 'react-redux'
 import * as AuthActions from '../actions/authentication';
 
-function NavBar({ panelType }) {
-
+function NavBar({ panelType, setPanelType }) {
   const dispatch = useDispatch();
   const history = useHistory();
   const profilePicUrl = useSelector((state) => state.authentication.user.pic_url);
+  const cartItems = useSelector(state => Object.values(state.cart.items));
   const [imageLoaded, setImageLoaded] = useState(false);
 
   const handleSubmit = async (e) => {
@@ -35,6 +35,19 @@ function NavBar({ panelType }) {
                 onLoad={() => setImageLoaded(true)}
               />
             </button>
+          </div>
+          <div className="navbar__cart-wrapper" onClick={() => setPanelType('cart')}>
+            <svg className="navbar__cart-svg" viewBox="0 0 21 17" xmlns="http://www.w3.org/2000/svg">
+              <g fill="none" fillRule="evenodd">
+                <g transform="translate(.56 .52)" fill="#192a32">
+                  <ellipse cx="14.753" cy="14.813" rx="1.366" ry="1.285"></ellipse>
+                  <ellipse cx="6.674" cy="14.813" rx="1.366" ry="1.285"></ellipse>
+                  <path d="M1.113 1.873h-.28a.832.832 0 0 1 0-1.665h2.83a1 1 0 0 1 .779.373l1.41 1.752a1 1 0 0 0 .779.373h12.106a1 1 0 0 1 .978 1.207l-1.365 6.422a1 1 0 0 1-.854.784L5.797 12.587a1 1 0 0 1-1.09-.736L2.508 3.583 2.03 2.476a1 1 0 0 0-.917-.603z"></path>
+                </g>
+                <path d="M0-2h21v21H0z"></path>
+              </g>
+            </svg>
+            {cartItems.length > 0 ? <span className="navbar__cart-count">{cartItems.length}</span> : null}
           </div>
         </div>
       : null
