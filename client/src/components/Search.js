7e319d9230f2d88ebab8c962d19fbab2950b7efe
Robert M. Estrada <57651772+robertestrada@users.noreply.github.com>
2020-11-02 21:30:25 -0800

    Create search files

---
+++ client/src/components/Search.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useHistory } from 'react-router-dom';
+import '../styles/search.css';
+
+const Search = ({ panelType, setPanelType }) => {
+
+  return (
+    <div className="search__wrapper">
+      
+    </div>
+  );
+}
+export default Search;
