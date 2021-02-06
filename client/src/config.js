2de21c917ec020ef782e7f9a3aadf11d549216f7
Robert M. Estrada <57651772+robertestrada@users.noreply.github.com>
2021-02-06 12:36:37 -0800

    Refactor env vars and add recaptcha backend routes

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,1 @@
 export const baseUrl = process.env.REACT_APP_BASE_URL || 'http://localhost:5000';
-export const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
-export const googleAPIKey = process.env.REACT_APP_GOOGLE_API_KEY;
-export const rcSiteKey = process.env.REACT_APP_RC_SITE_KEY || '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
